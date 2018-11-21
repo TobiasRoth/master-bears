a67eb49635deeb843a3e628687689242fe093af5
Tobias Roth <tobias@Mac-Tobi.local>
2018-11-21 22:56:38 +0100

    Erste Grafik gemacht, Response leicht angepasst.

--- R/Auswertung.R
+++ R/Auswertung.R
@@ -5,6 +5,7 @@ rm(list = ls(all = TRUE))
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Libraries
 library(tidyverse)
+library(broom)
 library(readxl)
 library(arm)
 
@@ -40,13 +41,30 @@ dat %>% filter(n_2 < resp_2)
 dat %>% filter(n_4 < resp_4)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# First model ---
+# Analyse proportion of events in 2m circle or contact zone ---
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# We have do decide how we want to measure the reaction of the salmons
+# For example the proportion of observations in the contact zone and the 2m circle
+mod <- glmer(cbind(n_0 + n_2, n_tot - n_0 - n_2) ~ treatment + (1|day), family = binomial, data = dat) 
+summary(mod)
+simres <- sim(mod, 100)@fixef
 
-# For example the proportion of observations in the contact zone
-glmer(cbind(n_0, n_tot-n_0) ~ treatment + (1|day), family = binomial, data = dat) %>% 
-  summary
+# Make plot for BB (intercept) vs WW
+BB <- quantile(plogis(simres[, "(Intercept)"]), probs = c(0.025, 0.5, 0.975))
+WW <- quantile(plogis(simres[, "(Intercept)"] + simres[, "treatmentWW"]), probs = c(0.025, 0.5, 0.975))
+pdf("Results/BBvsWW.pdf", width = 4, height = 5)
+par(mar = c(3, 4, 1, 1))
+plot(1:2, c(BB["50%"], WW["50%"]), pch = 16, axes = FALSE, 
+     ylim = c(0,0.5), ylab = "",
+     xlim = c(0.7, 2.3), xlab = "")
+segments(x0 = 1:2, y0 = c(BB["2.5%"], WW["2.5%"]), x1 = 1:2, y1 = c(BB["97.5%"], WW["97.5%"]))
+axis(1, 1:2, c("BB", "WW"), pos = 0)
+lines(c(0.7, 1), c(0, 0))
+lines(c(2, 2.3), c(0, 0))
+axis(2, las = 1, pos = 0.7)
+mtext(text = "Treatment", side = 1, line = 1.5)
+mtext(text = "Non-evasive behavior", side = 2, line = 3)
+mtext(text = "(proportion of events in 2m circle or contact zone)", side = 2, line = 2.2, cex = 0.8)
+dev.off()
 
 
 
