605f38e8d35a63bf25228d255fedf9b746d15598
Tobias Roth <tobias@Mac-Tobi.local>
2018-12-06 17:01:53 +0100

    Skript mit Vali und Jakob besprochen und ergänzt.

--- R/Auswertung.R
+++ R/Auswertung.R
@@ -9,42 +9,38 @@ library(broom)
 library(readxl)
 library(arm)
 
+dat <- 
+m <- mean(c(10, 20))
+
+c(10, 20) %>% 
+  mean()
+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Prepare data ---
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Read in data from Excel and change column names / data types
 dat <- read_excel("Data/Datasheet_Bear_2.0.xlsx") %>% 
-  transmute(day = Collection %>% as.integer,
+  transmute(day = Collection %>% as.integer(),
             rep = Replication %>% as.integer,
             observer = ID,
             treatment = Setup,
             n_fish = `Number Fish`,
-            n_0 = as.integer(`Contact`)  + as.integer(`Contact > 10s`),
-            n_2 = as.integer(`2m`)  + as.integer(`2m > 10s`),
-            n_4 = as.integer(`4m`)  + as.integer(`4m > 10s`),
-            n_tot = n_0 + n_2 + n_4,
-            resp_0 = as.integer(`Response Contact`),
-            resp_2 = as.integer(`Response 2m`),
-            resp_4 = as.integer(`Response 4m`))
+            total = as.integer(`Contact`)  + as.integer(`Contact > 10s`) + as.integer(`2m`) + 
+              as.integer(`2m > 10s`) + as.integer(`4m`)  + as.integer(`4m > 10s`) + as.integer(`Response Contact`) +
+              as.integer(`Response 2m`) + as.integer(`Response 4m`),
+            total_2m = as.integer(`Contact`)  + as.integer(`Contact > 10s`) + as.integer(`2m`) + 
+              as.integer(`2m > 10s`) + as.integer(`Response Contact`) +
+              as.integer(`Response 2m`),
+            Resp = `Response 2m` %>% as.integer())
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Check plausibility of data ---
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# The number of fishes should always be higher than the number of fishes counted
-# in the contact zone?
-dat %>% filter(n_fish < n_0)
 
-# The number of response should not be higher than the number of observations in
-# the respective circle?
-dat %>% filter(n_0 < resp_0)
-dat %>% filter(n_2 < resp_2)
-dat %>% filter(n_4 < resp_4)
+tapply(dat$Resp, dat$treatment, sum) %>% barplot(las = 1)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Analyse proportion of events in 2m circle or contact zone ---
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # For example the proportion of observations in the contact zone and the 2m circle
-mod <- glmer(cbind(n_0 + n_2, n_tot - n_0 - n_2) ~ treatment + (1|day), family = binomial, data = dat) 
+mod <- glmer(cbind(total_2m, total-total_2m) ~ treatment + (1|day), family = binomial, data = dat) 
 summary(mod)
 simres <- sim(mod, 100)@fixef
 
